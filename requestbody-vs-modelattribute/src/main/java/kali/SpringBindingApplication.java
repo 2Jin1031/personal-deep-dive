1a7d314607f126198c55a0dae0aa5089a0ac0074
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-16 10:41:19 +0900

    init: requestbody-vs-modelattribute 프로젝트 초기 세팅

---
+++ requestbody-vs-modelattribute/src/main/java/kali/SpringBindingApplication.java
@@ -0,0 +1,11 @@
+package kali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBindingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBindingApplication.class, args);
+    }
+}
