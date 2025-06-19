d4836f211f868d5de0f0f6e7e16960280dc0ca00
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-19 15:44:19 +0900

    init: JPA IDENTITY 전략의 즉시 INSERT와 1차 캐시 동작 검증 프로젝트

---
+++ persistencecontext-hibernategenerationtype/src/main/java/kali/JpaInsertTimingApplication.java
@@ -0,0 +1,11 @@
+package kali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaInsertTimingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaInsertTimingApplication.class, args);
+    }
+}
